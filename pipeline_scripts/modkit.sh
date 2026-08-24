62e45529aeb8be94891c02b152a790225b3a9687
Harit Kohli <kohliharit3@gmail.com>
2026-08-24 16:59:18 +0100

    Update modkit.sh

--- pipeline_scripts/modkit.sh
+++ pipeline_scripts/modkit.sh
@@ -1,4 +1,4 @@
-modkit dmr pair -a h1_pileup.bed.gz -b h2_pileup.bed.gz -o dmr_result.bed \
- -r /media/harit/biggie/fast5_ont/dmr_analysis/new_icr.bed \
- --ref /media/harit/biggie/fast5_ont/ref_genome/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
+modkit dmr pair -a h1_pileup.bed.gz -b h2_pileup.bed.gz -o dmr_result.bed \ # Two haplotype-split pileups created from split haplotagged BAMs
+ -r /media/harit/biggie/fast5_ont/dmr_analysis/new_icr.bed \  # List of iDMRs
+ --ref /media/harit/biggie/fast5_ont/ref_genome/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \ # Reference genome
  --base C --threads 16 --log-filepath dmr.log
